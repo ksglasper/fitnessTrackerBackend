4060ed5d2e7ec0aa29974762b33bce2ca3a212cb
Darius Robinson <robinson.darius090@gmail.com>
2022-07-25 11:30:09 -0400

    users fix

--- db/index.js
+++ db/index.js
@@ -1,37 +1,7 @@
 module.exports = {
- const client = require("./client")
-  // ...require('./client'), // adds key/values from users.js
-const {
-  createUser,
-   getUser, 
-   getUserById, 
-   getUserByUsername} = require('./users'), // adds key/values from users.js
-
-const {
-  getAllActivities,
-  createActivity, 
-  getActivityById,
-   getActivityByName,
-    attachActivitiesToRoutines,
-     updateActivity} = require('./activities'), // adds key/values from activites.js
-
-const {
-  getRoutineById,
-  getRoutinesWithoutActivities,
-  getAllRoutines,
-  getAllPublicRoutines,
-  getAllRoutinesByUser,
-  getPublicRoutinesByUser,
-  getPublicRoutinesByActivity,
-  createRoutine,
-  updateRoutine,
-  destroyRoutine} = require('./routines'), // etc
-  
-  const {
-    getRoutineActivityById,
-    addActivityToRoutine,
-    getRoutineActivitiesByRoutine,
-    updateRoutineActivity,
-    destroyRoutineActivity,
-    canEditRoutineActivity} = require('./routine_activities') // etc
+  ...require('./client'), // adds key/values from users.js
+  ...require('./users'), // adds key/values from users.js
+  ...require('./activities'), // adds key/values from activites.js
+  ...require('./routines'), // etc
+  ...require('./routine_activities') // etc
 }
