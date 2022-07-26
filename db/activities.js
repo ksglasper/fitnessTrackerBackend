c36f5ccf7a5c625c618782e6a8095ab0318a15a2
Darius Robinson <robinson.darius090@gmail.com>
2022-07-26 12:13:51 -0400

    working on update activity

--- db/activities.js
+++ db/activities.js
@@ -11,7 +11,7 @@ async function createActivity({ name, description }) {
   RETURNING *;
   `,[name, description])
 
-  console.log(activity, 'here is the activity please be an object')
+  // console.log(activity, 'here is the activity please be an object')
   return activity
   } catch (error) {
     console.error
@@ -27,17 +27,67 @@ async function getAllActivities() {
     return rows
   } catch (error) {
     console.error
-    
+    throw error
   }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const {rows: [activities]} = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE id=$1;
+    `,[id])
+    // console.log(activities.id, "this should be activities")
+    return activities
+  } catch (error) {
+    console.error
+    throw error
+  }
+}
 
-async function getActivityByName(name) {}
+async function getActivityByName(name) {
+  try {
+    const {rows: [activities]} = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE name=$1
+    `, [name])
+    return activities
+  } catch (error) {
+    console.error
+    throw error
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {}
 
-async function updateActivity({ id, ...fields }) {
+
+
+
+async function updateActivity({ id, ...fields}) {
+  console.log(id, "this is ID")
+  console.log(Object.keys(fields), 'this is fields')
+  console.log(Object.values(fields))
+  const setString = Object.keys(fields)
+  .map((key,idx)=>{`"${key}"=$${idx + 1}`})
+  .join(',');
+  console.log(setString, "this is SETSTRING")
+  try {
+    const {rows: activities} = await client.query(`
+    UPDATE activities
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `,Object.values(fields))
+    return activities
+  } catch (error) {
+    console.error
+    throw error
+  }
+  
+  
+  
   // don't try to update the id
   // do update the name and description
   // return the updated activity
