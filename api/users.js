48741b0edd3e4555474ef5e766ba5a7b7dbafc74
Kyle Glasper <ksglasper@gmail.com>
2022-07-28 13:00:38 -0500

    started on api users

--- api/users.js
+++ api/users.js
@@ -1,8 +1,32 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
+const {createUser, getUserByUsername } = require('../db/users')
 
+
+router.use((req, res, next)=>{
+    console.log('A request is being made to /users')
+    next()
+})
 // POST /api/users/register
+router.post('/register', async (res, req, next)=>{
+    const {username, password} = res.body
+
+    if(password.length < 8){
+        next({
+            name: 'PasswordShortError',
+            message: 'A password must be at least 8 characters long'
+
+        })
+    }
+
+
+
+    // createUser()
+
+})
+
+
 
 // POST /api/users/login
 
--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+// const client = require("./client");
+const apiRouter = require('./api')
+const morgan = require("morgan")
+
+app.use(morgan('dev'));
+
+app.use(express.json())
+
+
+app.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+  
+  next();
+});
+
+app.use('/api',apiRouter)
+
+app.get('/', (req, res) =>{
+    res.send(`
+    <h1>Welcome to the Homepage!</h1>
+    <a href='/api'>Head over to the API</a>
+    
+    `)
+    })
+
+
+
 
 // Setup your Middleware and API Router here
+
+
+
+
 
 module.exports = app;
